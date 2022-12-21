0bebd3cee73876d68cee5c9573649a7a21f6164d
Kiran Banger <kiranb004@gmail.com>
2022-12-20 18:09:21 -0800

    separated genres dropdown component, new incomplete add genres component displays when add new genre is selected in dropdown

---
+++ client/components/PlaylistFormAddGenre.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PlaylistFormAddGenre = (props) => {
+  return(
+    <div className='add-genre-container'>
+      <label className='genre-input'>
+        a label
+      </label>
+
+    </div>
+  )
+}
+
+export default PlaylistFormAddGenre;
--- client/components/PlaylistFormGenres.jsx
+++ client/components/PlaylistFormGenres.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PlaylistFormAddGenre from './PlaylistFormAddGenre.jsx';
 
 const PlaylistFormGenres = (props) => {
+  // if add new genre is selected display the PlaylistFormAddGenre component
+  const [dropdownValue, setDropdownValue] = useState('');
+
+  const handleChange = (e) => {
+    setDropdownValue(e.target.value)
+    console.log('changing value to ', e.target.value)
+  }
+
   return(
-    
+    <div>
     <div className='dropdown-container'>
       <label className='dropdown-label'>
         What genre are you in the mood for?<br/>
-        <select className='dropdown-menu'>
+        <select className='dropdown-menu' onChange={handleChange}>
           <option disabled selected>Select Your Genre</option>
           <option value="electronic">Electronic</option>
           <option value="k-pop">K-Pop</option>
@@ -16,6 +25,10 @@ const PlaylistFormGenres = (props) => {
           <option value="add-new-genre">Add New Genre</option>
         </select>
       </label>
+    </div>
+    <div>
+      {dropdownValue === 'add-new-genre' && <PlaylistFormAddGenre/>}
+    </div>
     </div>
   )
 }
